2c9923bfaa378d1506399f16ea475d0241b3c26f
xhugglesx <xhugglesx@gmail.com>
2014-01-25 20:43:24 -0500

    Enemy Garbage Collection Complete
    Enemies get destroyed when they leave the camera view.

---
+++ Assets/Scripts/DestroyByVisibility.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByVisibility : MonoBehaviour {
+
+	public bool hasBeenVisible;
+
+	void Start()
+	{
+		hasBeenVisible = false;
+	}
+
+	void OnBecameVisible () 
+	{
+		hasBeenVisible = true;
+	}
+
+	void OnBecameInvisible()
+	{
+		if (hasBeenVisible)
+			Destroy (gameObject);
+	}
+}
